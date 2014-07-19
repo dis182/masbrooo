c5b348d6cb38e54105b68aeb4a8cb9bcbe01e2e8
dis182 <adisthana.wijaya@gmail.com>
2014-07-19 22:45:10 +0800

    Commit

---
+++ Magazine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Magazine
+//
+//  Created by Adisthana Wijaya on 7/18/14.
+//  Copyright Gavoora 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
